7837bdd66ebaf27e75bc463dd2aad4b597b5027d
pl80tech <pl80.tech@gmail.com>
2019-03-19 09:18:24 +0900

    Define a function to get log data

--- log/data_visualize.py
+++ log/data_visualize.py
@@ -3,20 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+def getLogData(logfile):
+	logline = open(logfile, "r").readlines()
+	cte = [0] * len(logline)
+	x = [0] * len(logline)
+
+	for i in range(len(logline)):
+		cte[i] = float(logline[i].strip('\n'))
+		x[i] = i
+
+	return x, cte
+
 if len(sys.argv) > 1:
 	logfile = sys.argv[1]
 	print("Log file to be visualized: " + str(logfile))
 else:
 	print("No file to be visualized")
 
-logline = open(logfile, "r").readlines()
-
-cte = [0] * len(logline)
-x = [0] * len(logline)
-
-for i in range(len(logline)):
-    cte[i] = float(logline[i].strip('\n'))
-    x[i] = i
+x, cte = getLogData(logfile)
 
 plt.plot(x, cte)
 

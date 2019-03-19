3275202204516512d3a8ec2e96e6ca273eaa8e98
pl80tech <pl80.tech@gmail.com>
2019-03-19 10:10:43 +0900

    Add some explanation comments

--- log/data_visualize.py
+++ log/data_visualize.py
@@ -1,8 +1,10 @@
+# Import the required packages
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+# Get the data from log and set x, y coordinate
 def getLogData(logfile):
 	logline = open(logfile, "r").readlines()
 	cte = [0] * len(logline)
@@ -14,6 +16,7 @@ def getLogData(logfile):
 
 	return x, cte
 
+# Set x, y coordinate for center line
 def getCenterLine(len):
 	x = [0] * len
 	y = [0] * len
@@ -23,18 +26,21 @@ def getCenterLine(len):
 
 	return x, y
 
+# Get the log file from command line
 if len(sys.argv) > 1:
 	logfile = sys.argv[1]
 	print("Log file to be visualized: " + str(logfile))
 else:
 	print("No file to be visualized")
 
+# Plot the data from log file
 x, cte = getLogData(logfile)
 center_x, center_y = getCenterLine(len(x))
 
 plt.plot(x, cte)
 plt.plot(center_x, center_y)
 
+# Save visualized data to image file
 if len(sys.argv) > 2:
 	savefile = sys.argv[2]
 	print("Save visualized data to " + str(savefile))
@@ -42,4 +48,5 @@ if len(sys.argv) > 2:
 else:
 	print("Not save the visualized data")
 
+# Show on screen
 plt.show()

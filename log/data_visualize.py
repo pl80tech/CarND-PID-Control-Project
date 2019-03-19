29856d1ea8694c01555a0f69a5767c6d6ddd6501
pl80tech <pl80.tech@gmail.com>
2019-03-19 14:33:47 +0900

    Align the number of iterations to be visualized

--- log/data_visualize.py
+++ log/data_visualize.py
@@ -31,14 +31,16 @@ x1, cte1 = getLogData(sys.argv[1])
 x2, cte2 = getLogData(sys.argv[2])
 x3, cte3 = getLogData(sys.argv[3])
 
+# Number of iterations to be visualized
+n_iteration = min(len(x1), len(x2), len(x3))
+
 # Get x, y coordinate for center line
-center_len = max(len(x1), len(x2), len(x3))
-center_x, center_y = getCenterLine(center_len)
+center_x, center_y = getCenterLine(n_iteration)
 
 # Plot multiple data in a same figure
-plt.plot(x1, cte1)
-plt.plot(x2, cte2)
-plt.plot(x3, cte3)
+plt.plot(x1[0:n_iteration-1], cte1[0:n_iteration-1])
+plt.plot(x2[0:n_iteration-1], cte2[0:n_iteration-1])
+plt.plot(x3[0:n_iteration-1], cte3[0:n_iteration-1])
 plt.plot(center_x, center_y)
 
 # Add title, label and legend
